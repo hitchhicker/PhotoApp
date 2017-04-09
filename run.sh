e2fe0439f301002737bfe00255a67c4d258643a6
hitchhicker <yubokai8@gmail.com>
2017-04-09 13:45:01 +0200

    Modify README

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 docker run -d --net=host \
 -v $(pwd)/codes:/var/www/photoApp/codes \
 -v $(pwd)/logs:/var/www/photoApp/logs \
-first_blood
+photoApp
 
 ## for Ubuntu ##
 # docker run -d -p 80:80 \
 # -v $(pwd)/codes:/var/www/photoApp/codes \
 # -v $(pwd)/logs:/var/www/photoApp/logs \
-# test
+# photoApp
