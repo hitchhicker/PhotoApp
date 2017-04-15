e40e17d0274d775a4ea04ab1dfcb174beffabcd8
hitchhicker <yubokai8@gmail.com>
2017-04-15 21:35:32 +0200

    remove port mapping in test_flask.sh

--- test_flask.sh
+++ test_flask.sh
@@ -4,7 +4,7 @@
 #-v $(pwd)/logs:/var/www/photoApp/logs \
 #photoapp /bin/bash -c "/etc/init.d/postgresql start; py.test -v codes/flask/test"
 
-docker run -p 80:80 \
+docker run \
 -v $(pwd)/codes:/var/www/photoApp/codes \
 -v $(pwd)/logs:/var/www/photoApp/logs \
 photoapp /bin/bash -c "/etc/init.d/postgresql start; py.test -v codes/flask/test"
