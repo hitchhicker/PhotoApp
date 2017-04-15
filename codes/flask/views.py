15c8e1e8b1407dbbde50cfdc0d39d7b72a82ee8b
hitchhicker <yubokai8@gmail.com>
2017-04-15 19:46:07 +0200

    remove falsk-cors

--- codes/flask/views.py
+++ codes/flask/views.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, request
-from flask.ext.cors import cross_origin
 
 from user_admin import UserAdmin
 
 
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def index():
     return render_template('index.html')
 
 
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def register():
     name = request.values.get('name')
     email = request.values.get('email')
@@ -19,7 +16,6 @@ def register():
     return user_admin.register()
 
 
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def login():
     email = request.values.get('email')
     password = request.values.get('password')
